14f44226b3471be2a6c80dfa084c816d384e9375
Andrey <andrey.zalozhenkov@pikta.ru>
2023-10-06 13:28:55 +0400

    Добавьте файлы проекта.

---
+++ 02.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+
+template<typename T>
+void print_container(const T& cont)
+{
+	std::ostream_iterator<std::string> it{std::cout, " "};
+	std::copy(cont.begin(), cont.end(), it);
+	std::cout << std::endl;
+}
+
+int main()
+{
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set);
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list);
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector);
+
+	return 0;
+}
